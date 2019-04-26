0aabe36b36dc9c3196623a2bde56fb9547d20cab
dgolicher <dgolicher@gmail.com>
2019-04-26 17:20:27 +0000

    Initial commit

--- R/giscourse.R
+++ R/giscourse.R
@@ -9,24 +9,24 @@
 #' conn<-connect()
 #'
 
-connect<-function(){
+connect<-function(db="gis_course"){
 
   library(RPostgreSQL)
   library(maptools)
   library(raster)
   conn <- dbConnect("PostgreSQL", host = "postgis",
-  dbname = "gis_course" ,user = "gis_course", password = "gis_course123")
+  dbname = db ,user = "gis_course", password = "gis_course123")
   return(conn)
 }
 
 
 
-sconnect<-function(pwd="docker"){
+sconnect<-function(pwd="docker", db="gis_course"){
   library(RPostgreSQL)
   library(maptools)
   library(raster)
   conn <- dbConnect("PostgreSQL", host = "postgis",
-                    dbname = "gis_course" ,user = "docker", password = pwd)
+                    dbname = db ,user = "docker", password = pwd)
   return(conn)
 }
 
