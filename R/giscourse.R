8a250eb619835325c7b2570d505233ed2223c07b
dgolicher <dgolicher@gmail.com>
2019-04-26 14:02:57 +0000

    Initial commit

--- R/giscourse.R
+++ R/giscourse.R
@@ -1,4 +1,3 @@
-hello<-function()
 
 connect<-function(){
   library(RPostgreSQL)
@@ -13,6 +12,23 @@ qmap<-function(place="Bournemouth"){
   g<-tmaptools::geocode_OSM(place)
   mapview::mapview(g$bbox)
 }
+
+hansen_wms<-function(m){
+  require(leaflet.extras)
+  require(mapview)
+  m@map %>% addFullscreenControl() %>% addMiniMap(position = "bottomleft",zoomLevelOffset = -4, toggleDisplay = TRUE) %>%
+    addWMSTiles(group="Forest 2000",
+                "http://r.bournemouth.ac.uk:8083/AG/wms",
+                layers = "AG:forest2000",
+                options = WMSTileOptions(format = "image/png", transparent = TRUE)) %>%
+    addWMSTiles(group="Forest loss",
+                "http://r.bournemouth.ac.uk:8083/AG/wms",
+                layers = "AG:hansen_loss",
+                options = WMSTileOptions(format = "image/png", transparent = TRUE)) %>%
+    mapview:::mapViewLayersControl (names = c("Forest 2000","Forest loss")) -> mymap
+  return(mymap)}
+
+
 
 emap<-function(place="Bournemouth")
 {
